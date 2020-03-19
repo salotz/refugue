4c1e6dd432d5e3096204d4bf65db2a7d3fb4e6a8
Samuel Lotz <samuel.lotz@salotz.info>
2020-03-19 00:38:34 -0400

    working on domain modelling and mocking out main things

--- src/refugue/cli.py
+++ src/refugue/cli.py
@@ -14,35 +14,56 @@ from .main import (
 import refugue.config as config
 
 
-_PROTOCOL_MAPS = {
-    'rsync' : RsyncProtocol,
-}
+_PROTOCOL_MAPS = (
+    ('rsync', RsyncProtocol),
+)
 
 @click.command()
 @click.option("--config", type=click.Path(exists=True), default="$HOME/.refugue/config.py")
+@click.option("--sync-file", type=click.Path(exists=True), default="$HOME/.refugue/sync_specs.toml")
+@click.option("--sync", default=None)
+@click.option("--transport", default=None)
 @click.option("--protocol", default='rsync')
 @click.argument("src")
 @click.argument("target")
-def cli(config, protocol, src, target):
+def cli(config, sync_file, sync, protocol, src, target):
+
+    # TODO: calidate options
 
     # expand shell variables to the config file
     config = Path(osp.expanduser(osp.expandvars(config)))
 
-    # use runpy to get the declared variables as a dictionary
+    # TODO: use runpy to get the declared variables as a dictionary
+    config_d = foo()
 
-    network = Network
+    # TODO: conjure up a local invoke context to run from
+    local_cx = foo()
+
+    # TODO: build the network from the configuration file
+    network = Network(**config_d)
+
+    # TODO: do the network discovery to find
+    network.discovery(cx)
 
     src_replica = Replica(src)
     target_replica = Replica(target)
 
-    sync_protocol = _PROTOCOL_MAPS[protocol]
-
-    sync_pair = SyncPair(
-        src=src_replica,
-        target=target_replica,
+    # identify the replicas in the network, discover current network
+    # topology, validate connection viability, and reify
+    sync_pair = network.pair(
+        src_replica,
+        target_replica,
     )
 
-    sync_pair.sync(sync_protocol)
+
+    # choose protocol
+    sync_protocol = dict(_PROTOCOL_MAPS)[protocol]
+
+    sync_pair.sync(
+        cx,
+        sync_protocol,
+        
+    )
 
 if __name__ == "__main__":
 
